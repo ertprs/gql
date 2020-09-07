bbdcc35a061624bc66e56c4c52209486247f4c83
Leo Falco <leonardo.falco@outlook.com>
2020-09-07 17:34:22 -0300

    sentry release and heroku deploy

--- bin/sentry-release.sh
+++
@@ -1,7 +0,0 @@
-export SENTRY_AUTH_TOKEN=605dbd2984e449d79308e0777137d30f1b1fdfb6f8314b588915e43c09e3547a
-export SENTRY_ORG=leofalco
-
-VERSION=$(git describe --abbrev=0 --tags)
-sentry-cli releases new -p gql $VERSION --log-level=info
-sentry-cli releases set-commits --auto $VERSION hero--log-level=info
-sentry-cli releases finalize $VERSION --log-level=info
